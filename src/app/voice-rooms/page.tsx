cc686c01b14ea8b62ee21805f881966d444903f0
ctrlshift-hash <booleansolana@gmail.com>
2025-11-03 03:43:51 +0000

    Fix voice rooms page to use client component and add missing import

--- src/app/voice-rooms/page.tsx
+++ src/app/voice-rooms/page.tsx
@@ -1,38 +1,20 @@
+"use client";
+
 import RoomList from "@/components/voice/RoomList";
 import Layout from "@/components/layout/Layout";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { prisma } from "@/lib/prisma";
+import { useSession } from "next-auth/react";
 
-export default async function VoiceRoomsPage() {
-  const session = await getServerSession(authOptions);
-  
-  // Get user data for Layout
-  let user = null;
-  if (session?.user?.id) {
-    const dbUser = await prisma.user.findUnique({
-      where: { id: session.user.id },
-      select: {
-        id: true,
-        username: true,
-        walletAddress: true,
-        isVerified: true,
-        profileImage: true,
-      },
-    });
-    if (dbUser) {
-      user = {
-        id: dbUser.id,
-        username: dbUser.username || session.user.username || "user",
-        walletAddress: dbUser.walletAddress ?? undefined,
-        isVerified: dbUser.isVerified || false,
-        profileImage: dbUser.profileImage ?? undefined,
-      };
-    }
-  }
+export default function VoiceRoomsPage() {
+  const { data: session } = useSession();
   
   return (
-    <Layout user={user}>
+    <Layout user={session?.user ? {
+      id: session.user.id,
+      username: session.user.username || "",
+      walletAddress: undefined,
+      isVerified: session.user.isVerified || false,
+      profileImage: undefined,
+    } : null}>
       <RoomList />
     </Layout>
   );
